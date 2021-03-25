e4c865936ff0d1674fee9429802a737010d2b406
victoriapoltronieri <65969480+victoriapoltronieri@users.noreply.github.com>
2021-03-25 10:47:03 -0300

    i quit

---
+++ Candidato.java
@@ -0,0 +1,235 @@
+package src;
+
+import java.util.Comparator;
+
+class ComparadorUrnaCandidato implements Comparator<Candidato> {
+
+    @Override
+    public int compare(Candidato cand1, Candidato cand2) {
+        if(cand1 != null){
+            if(cand1.getVotos_nominais() > cand2.getVotos_nominais()){
+                return -1;
+            }
+            else if(cand1.getVotos_nominais() < cand2.getVotos_nominais()){
+                return 1;
+            }else{
+                if(cand1.getNumero_partido() < cand2.getNumero_partido()){
+                    return -1;
+                }else{
+                    return 1;
+                }
+            }
+        }else{
+            return 0;
+        }
+        
+    }
+
+}
+
+public class Candidato implements Comparable<Candidato>{
+    private String nome;
+    private char sexo; // F ou M
+    private String data_nasc; // dd/mm/aaaa;    
+    private int numero;
+    private int numero_partido;
+    private int votos_nominais;
+    private String situacao; // Eleito, não eleito, suplente
+    private String nome_urna;
+    private String destino_voto; // Válido​,​Anulado ou ​Anulado subjudice​
+
+    public Candidato(int numero, int votos_nominais, String situacao, String nome, String nome_urna, char sexo, String data_nasc, String destino_voto, int numero_partido){
+        setNumero(numero);
+        setVotos_nominais(votos_nominais);
+        setSituacao(situacao);
+        setNome(nome);
+        setNome_urna(nome_urna);
+        setSexo(sexo);
+        setData_nasc(data_nasc);
+        setDestino_voto(destino_voto);
+        setNumero_partido(numero_partido);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    public String getData_nasc() {
+        return data_nasc;
+    }
+    public void setData_nasc(String data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public int getNumero_partido() {
+        return numero_partido;
+    }
+    public void setNumero_partido(int numero_partido) {
+        this.numero_partido = numero_partido;
+    }
+    public int getVotos_nominais() {
+        return votos_nominais;
+    }
+    public void setVotos_nominais(int votos_nominais) {
+        this.votos_nominais = votos_nominais;
+    }
+    public String getSituacao() {
+        return situacao;
+    }
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+    public String getNome_urna() {
+        return nome_urna;
+    }
+    public void setNome_urna(String nome_urna) {
+        this.nome_urna = nome_urna;
+    }
+    public String getDestino_voto() {
+        return destino_voto;
+    }
+    public void setDestino_voto(String destino_voto) {
+        this.destino_voto = destino_voto;
+    }
+
+    public boolean identificaEleitos(){
+        return this.getSituacao().equals("Eleito");
+    }
+
+    public boolean identificaValidade(){
+        return this.getDestino_voto().equals("Válido");
+    }
+
+    public String toString(Partido[] part){
+        for(int i = 0; i < part.length; i++){
+            if(part[i].comparaPartido(this.numero_partido)){
+                String print = this.getNome() + " / " + this.getNome_urna() + " (" + part[i].getSigla_partido() + ", " + Integer.toString(this.getVotos_nominais());
+                StringBuilder s = new StringBuilder(print);
+                if(this.getVotos_nominais() <= 1){
+                    s.append(" voto)");
+                }
+                else{
+                    s.append(" votos)");
+                }
+                print = s.toString();
+                return print;
+            }
+        }
+        return "erro";
+    }
+
+    public String toString(Partido[] part, String nome, int num, int votos){
+        for(int i = 0; i < part.length; i++){
+            if(part[i].comparaPartido(this.numero_partido)){  
+                String print = part[i].getSigla_partido() + " - " + this.getNumero_partido() + ", " + this.getNome_urna() + " (" + this.getNumero() + ", " + this.getVotos_nominais();
+                StringBuilder s = new StringBuilder(print);
+                if(this.getVotos_nominais() <= 1){
+                    s.append(" voto) / " + nome + " (" + num + ", " + votos);
+                }
+                else{
+                    s.append(" votos) / " + nome + " (" + num + ", " + votos);
+                }
+                
+                if(votos <= 1){
+                    s.append(" voto)");
+                }
+                else{
+                    s.append(" votos)");
+                }
+                
+                print = s.toString();
+                return print;
+            }
+        }
+        return "erro";
+    }
+
+    public int compareTo(Candidato cand) {
+        if(cand != null){
+            String[] dataCand1 = new String[3];
+            String[] dataCand2 = new String[3];
+            
+            dataCand1 = this.data_nasc.split("/");
+            dataCand2 = cand.getData_nasc().split("/");
+            
+            if(this.votos_nominais > cand.getVotos_nominais()){
+                return -1;
+            }
+            else if(this.votos_nominais < cand.getVotos_nominais()){
+                return 1;
+            }
+            else{
+                if(Integer.parseInt(dataCand1[2]) < Integer.parseInt(dataCand2[2])){ // retorna -1, o cand1 é mais velho
+                    return -1;
+                }
+                else if(Integer.parseInt(dataCand1[2]) > Integer.parseInt(dataCand2[2])){ // retorna 1, o cand2 é o mais velho
+                    return 1;
+                }
+                else{
+                    if(Integer.parseInt(dataCand1[1]) < Integer.parseInt(dataCand2[1])){ // retorna -1, o cand1 é mais velho
+                        return -1;
+                    }
+                    else if(Integer.parseInt(dataCand1[1]) > Integer.parseInt(dataCand2[1])){ // retorna 1, o cand2 é o mais velho
+                        return 1;
+                    }
+                    else{
+                        if(Integer.parseInt(dataCand1[0]) < Integer.parseInt(dataCand2[0])){ // retorna -1, o cand1 é mais velho
+                            return -1;
+                        }
+                        else if(Integer.parseInt(dataCand1[0]) > Integer.parseInt(dataCand2[0])){ // retorna 1, o cand2 é o mais velho
+                            return 1;
+                        }
+                        else{
+                            if(this.getNumero() < cand.getNumero()){
+                                return -1;
+                            }
+                            else{
+                                return 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public boolean comparaNumPartido(int numeroId){
+        if(this.numero_partido == numeroId){
+            return true;
+        }
+        return false;
+    }
+
+    public int retornaIdadeCandidato(String dataRef){
+        String[] dataCand = new String[3];
+        String[] dataAtual = new String[3];
+
+        dataCand = this.data_nasc.split("/");
+        dataAtual = dataRef.split("/");
+        if(Integer.parseInt(dataCand[1]) < Integer.parseInt(dataAtual[1])){
+            return (Integer.parseInt(dataAtual[2]) - Integer.parseInt(dataCand[2]));
+        }else if(Integer.parseInt(dataCand[1]) == Integer.parseInt(dataAtual[1]) && Integer.parseInt(dataCand[0]) <= Integer.parseInt(dataAtual[0])){
+            return (Integer.parseInt(dataAtual[2]) - Integer.parseInt(dataCand[2]));
+        }else{
+            return (Integer.parseInt(dataAtual[2]) - Integer.parseInt(dataCand[2]) - 1);
+        }
+        
+    }
+    
+}
+
+
---
+++ Partido.java
@@ -0,0 +1,118 @@
+package src;
+
+public class Partido implements Comparable<Partido>{
+    private int numero_partido;
+    private int votos_legenda;
+    private String nome_partido;
+    private String sigla_partido;
+    private int votos_nominais;
+    private int votos_total;
+    private int cand_eleitos;
+
+    public Partido(int numero_partido, int votos_legenda, String nome_partido, String sigla_partido){
+        setNumero_partido(numero_partido);
+        setVotos_legenda(votos_legenda);
+        setNome_partido(nome_partido);
+        setSigla_partido(sigla_partido);
+        setVotos_total(0);
+        setVotos_nominais(0);
+        setCand_eleitos(0);
+    }
+
+    public int getNumero_partido() {
+        return numero_partido;
+    }
+    public void setNumero_partido(int numero_partido) {
+        this.numero_partido = numero_partido;
+    }
+    public int getVotos_legenda() {
+        return votos_legenda;
+    }
+    public void setVotos_legenda(int votos_legenda) {
+        this.votos_legenda = votos_legenda;
+    }
+    public String getNome_partido() {
+        return nome_partido;
+    }
+    public void setNome_partido(String nome_partido) {
+        this.nome_partido = nome_partido;
+    }
+    public String getSigla_partido() {
+        return sigla_partido;
+    }
+    public void setSigla_partido(String sigla_partido) {
+        this.sigla_partido = sigla_partido;
+    }
+    public int getVotos_total() {
+        return votos_total;
+    }
+    public void setVotos_total(int votos_total) {
+        this.votos_total = this.votos_nominais + this.votos_legenda;
+    }
+    public int getVotos_nominais() {
+        return votos_nominais;
+    }
+    public void setVotos_nominais(int votos_nominais) {
+        this.votos_nominais = votos_nominais;
+    }
+    public int getCand_eleitos() {
+        return cand_eleitos;
+    }
+    public void setCand_eleitos(int cand_eleitos) {
+        this.cand_eleitos = cand_eleitos;
+    }
+    
+    public boolean comparaPartido(int numeroId){
+        if(this.numero_partido == numeroId){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int compareTo(Partido part) {
+        if(this.votos_total > part.getVotos_total()){
+            return -1;
+        }
+        else if(this.votos_total < part.getVotos_total()){
+            return 1;
+        }else{
+            if(this.numero_partido < part.getNumero_partido()){
+                return -1;
+            }else{
+                return 1;
+            }
+        }
+    }
+    
+    public String toString(){
+        String print = this.sigla_partido + " - " + this.numero_partido + ", " + Integer.toString(this.getVotos_total());
+        StringBuilder result = new StringBuilder(print);
+          
+        if(this.getVotos_total() <= 1){
+           result.append(" voto (");
+        }
+        else{
+            result.append(" votos (");
+        }
+
+        result.append(Integer.toString(this.getVotos_nominais()));
+        if(this.getVotos_nominais() <= 1){
+            result.append(" nominal e ");
+        }
+        else{
+            result.append(" nominais e ");
+        }
+
+        result.append(Integer.toString(this.getVotos_legenda()) + " de legenda), " + Integer.toString(this.getCand_eleitos()));
+        if(this.getCand_eleitos() <= 1){
+            result.append(" candidato eleito");
+        }
+        else{
+            result.append(" candidatos eleitos");
+        }
+        print = result.toString();
+        return print;
+    }
+    
+}
